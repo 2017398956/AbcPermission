52aa294051ff1964092b5832f3d9e7b3b55778e8
2017398956 <2017398956@qq.com>
2020-09-23 17:19:19 +0800

    存在问题：当 clean project 后 多次 点击 run 按钮，kotlin下的 aop 失效，原因是：kotlin 的 class 文件重新生成了，待解决

--- permissionPlugin/src/main/groovy/personal/nfl/permission/plugin/AbcPermissionPlugin.groovy
+++ permissionPlugin/src/main/groovy/personal/nfl/permission/plugin/AbcPermissionPlugin.groovy
@@ -13,7 +13,7 @@ class AbcPermissionPlugin implements Plugin<Project> {
 
     private String sourceJDK = "1.8"
     private String targetJDK = "1.8"
-    private String abcPermissionVersion = "1.6.9"
+    private String abcPermissionVersion = "1.7.1"
     /**
      * @param project 这里的 project 对应的引入该插件的 module ，如果需要获取根目录下 build.gradle
      * 中的信息，则需要通过 project.rootProject.XXX 来获取
@@ -39,9 +39,15 @@ class AbcPermissionPlugin implements Plugin<Project> {
 //                exclude module: 'permissionSupport'
 //            }
 //        }
+        project.dependencies.add("compileOnly",
+                "com.github.2017398956:AbcPermission:${abcPermissionVersion}", {
+            "exclude module: 'permissionSupport'"
+            "exclude module: 'permissionCompiler'"
+        })
 
         // 默认使用 java 的注解方式
         String annotationMethod = "annotationProcessor"
+
         if (project.plugins.findPlugin("kotlin-kapt") != null) {
             // 支持 kotlin
             // 如果在 kotlin 代码中使用了注解的方式生成代码，那么需要在
@@ -50,6 +56,18 @@ class AbcPermissionPlugin implements Plugin<Project> {
             annotationMethod = "kapt"
         } else {
             // 使用默认的 annotationProcessor
+            // 如果 project.rootProject.buildScript 中引用了 kotlin 那么，自动添加上 kotlin-android 和 kotlin-kapt
+            project.rootProject.buildscript.configurations.each {
+                it.dependencies.each {
+                    if ("org.jetbrains.kotlin".equals(it.group) && "kotlin-gradle-plugin".equals(it.name)) {
+                        if (project.plugins.findPlugin("kotlin-android") == null) {
+                            project.pluginManager.apply("kotlin-android")
+                        }
+                        project.pluginManager.apply("kotlin-kapt")
+                        annotationMethod = "kapt"
+                    }
+                }
+            }
         }
         project.dependencies.add(annotationMethod,
                 "com.github.2017398956:AbcPermission:${abcPermissionVersion}", {
@@ -59,11 +77,6 @@ class AbcPermissionPlugin implements Plugin<Project> {
         project.dependencies.add("implementation",
                 "com.github.2017398956:AbcPermission:${abcPermissionVersion}", {
             "exclude module: 'permissionAnnotation'"
-            "exclude module: 'permissionCompiler'"
-        })
-        project.dependencies.add("compileOnly",
-                "com.github.2017398956:AbcPermission:${abcPermissionVersion}", {
-            "exclude module: 'permissionSupport'"
             "exclude module: 'permissionCompiler'"
         })
 
@@ -85,16 +98,16 @@ class AbcPermissionPlugin implements Plugin<Project> {
         }
     }
 
-    private void doFirst(Task javaCompile){
+    private void doFirst(Task javaCompile) {
         javaCompile.doFirst {
             // 获取 jdk 的版本
             if (project.hasProperty('android') && project.android != null) {
                 if (project.android.hasProperty('compileOptions') && project.android.compileOptions != null) {
                     if (project.android.compileOptions.hasProperty('targetCompatibility') && project.android.compileOptions.targetCompatibility != null) {
-                         targetJDK = project.android.compileOptions.properties.get('targetCompatibility')
+                        targetJDK = project.android.compileOptions.properties.get('targetCompatibility')
                     }
                     if (project.android.compileOptions.hasProperty('sourceCompatibility') && project.android.compileOptions.sourceCompatibility != null) {
-                         sourceJDK = project.android.compileOptions.properties.get('sourceCompatibility')
+                        sourceJDK = project.android.compileOptions.properties.get('sourceCompatibility')
                     }
                 }
             }
@@ -124,7 +137,7 @@ class AbcPermissionPlugin implements Plugin<Project> {
                                   "-d", dPath,
                                   "-classpath", classpath,
                                   "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)]
-            // println(javacArgs)
+            println("AbcPermission: aspectj args's sourceCompatibility is " + sourceJDK + " and targetCompatibility is " + targetJDK)
             new Main().run(javacArgs, handler)
             File[] kotlinClassFiles = FileUtil.listFiles(kotlinInPath, true)
             File javacKotlinFile
